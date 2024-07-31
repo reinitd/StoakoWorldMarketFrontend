6a029bcbe265a8f5a1f614e11de14c9dda7382e7
reinitd <russian.devv@gmail.com>
2024-07-31 03:30:46 -0700

    added all emojis

--- ts/populate/catalogEntryInfo.ts
+++ ts/populate/catalogEntryInfo.ts
@@ -6,6 +6,18 @@ function extractTextFromHtml(html: string): string {
     return doc.body.textContent || '';
 }
 
+const emoteMap = {
+    ':smile:': '<img src="/assets/emotes/smile.gif" alt="smiley"/>',
+    ':metal:': '<img src="/assets/emotes/metal.gif" alt="metal"/>',
+    ':thumbup:': '<img src="/assets/emotes/thumbup.gif" alt="thumbup"/>',
+    ':thumbdown:': '<img src="/assets/emotes/thumbdown.gif" alt="thumbdown"/>',
+    ':banghead:': '<img src="/assets/emotes/banghead.gif" alt="banghead"/>',
+    ':feedback:': '<img src="/assets/emotes/feedback.gif" alt="feedback"/>',
+    ':crazy:': '<img src="/assets/emotes/crazy.gif" alt="crazy"/>',
+    ':biggrin:': '<img src="/assets/emotes/biggrin.gif" alt="biggrin"/>',
+    ':beer:': '<img src="/assets/emotes/beer.gif" alt="beer"/>',
+    ':party:': '<img src="/assets/emotes/party.gif" alt="party"/>',
+};
 
 async function populateCatalogEntryInfo(uuid:string) : Promise<Result<CatalogEntry>> {
     let result = new Result<CatalogEntry>(false, "An unhandled error occured.", null);
@@ -30,11 +42,10 @@ async function populateCatalogEntryInfo(uuid:string) : Promise<Result<CatalogEnt
         element.textContent = ce.location;
     });
     document.querySelectorAll('.js-description-here').forEach(element => {
-        let desc = extractTextFromHtml(ce.description)
-            .replace(':smile:', '<img src="/assets/emotes/smile.gif" alt="smiley"/>')
-            .replace(':metal:', '<img src="/assets/emotes/metal.gif" alt="metal"/>')
-            .replace(':thumbup:', '<img src="/assets/emotes/thumbup.gif" alt="thumbup"/>')
-            .replace(':banghead:', '<img src="/assets/emotes/banghead.gif" alt="banghead"/>');
+        let desc = extractTextFromHtml(ce.description);
+        for (const [key, value] of Object.entries(emoteMap)) {
+            desc = desc.replace(new RegExp(key, 'g'), value);
+        }
         element.innerHTML = desc;
     });
     document.querySelectorAll('.js-category-link-here').forEach(element => {
